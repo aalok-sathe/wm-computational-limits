8b5fa7d44773f4b0f1a0af17cf6499bbe9b4c126
aalok-sathe <aalok@brown.edu>
2026-06-11 13:48:49 -0400

    minor, documentation stuff

--- src/workingmem/task/__init__.py
+++ src/workingmem/task/__init__.py
@@ -1,2 +1,12 @@
+"""
+include: ./README.md
+"""
+
 from workingmem.task.SIR.SIR import SIRDataset, SIRConfig
 from workingmem.task.interface import _T_dataset_or_collection_of_datasets
+
+__all__ = [
+    "SIRDataset",
+    "SIRConfig",
+    "_T_dataset_or_collection_of_datasets",
+]
